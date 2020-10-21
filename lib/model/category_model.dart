4a0bb2e7b4332ebab76f91c6ca3ab60dc5a84501
digitbase <digitbase@gmail.com>
2020-10-21 11:11:11 +0800

    分类-大类

---
+++ lib/model/category_model.dart
@@ -0,0 +1,100 @@
+import "../element/r_log.dart";
+
+class CategoryModel {
+  String code;
+  String message;
+  List<dynamic> data;
+
+  CategoryModel({this.code, this.message, this.data});
+
+  factory CategoryModel.fromJson(Map<String, dynamic> json) {
+    return CategoryModel(
+      code: json['code'],
+      message: json['message'],
+      data: json['data'] != null
+          ? json['data'].map((v) => new DataCategory.fromJson(v)).toList()
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['message'] = this.message;
+    if (this.data != null) {
+      data['data'] = this.data.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class DataCategory {
+  String mallCategoryId;
+  String mallCategoryName;
+  List<dynamic> bxMallSubDto;
+  String comments;
+  String image;
+
+  DataCategory(
+      {this.mallCategoryId,
+      this.mallCategoryName,
+      this.bxMallSubDto,
+      this.comments,
+      this.image});
+
+  factory DataCategory.fromJson(Map<String, dynamic> json) {
+    RLogger.instance.d(json['mallCategoryName'], tag: 'DataCategory tag');
+    return DataCategory(
+      mallCategoryId: json['mallCategoryId'],
+      mallCategoryName: json['mallCategoryName'],
+      bxMallSubDto: json['bxMallSubDto'] != null
+          ? json['bxMallSubDto']
+              .map((v) => new BxMallSubDto.fromJson(v))
+              .toList()
+          : null,
+      comments: json['comments'],
+      image: json['image'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> dataCategory = new Map<String, dynamic>();
+    dataCategory['mallCategoryId'] = this.mallCategoryId;
+    dataCategory['mallCategoryName'] = this.mallCategoryName;
+    if (this.bxMallSubDto != null) {
+      dataCategory['bxMallSubDto'] =
+          this.bxMallSubDto.map((v) => v.toJson()).toList();
+    }
+    dataCategory['comments'] = this.comments;
+    dataCategory['image'] = this.image;
+    return dataCategory;
+  }
+}
+
+class BxMallSubDto {
+  String mallSubId;
+  String mallCategoryId;
+  String mallSubName;
+  String comments;
+
+  BxMallSubDto(
+      {this.mallSubId, this.mallCategoryId, this.mallSubName, this.comments});
+
+  BxMallSubDto.fromJson(Map<String, dynamic> json) {
+    //RLogger.instance.d(json.toString(), tag: 'new tag');
+    mallSubId = json['mallSubId'];
+    mallCategoryId = json['mallCategoryId'];
+    mallSubName = json['mallSubName'];
+    comments = json['comments'];
+  }
+
+  Map<String, dynamic> toJson() {
+    //RLogger.instance.d('toJson', tag: 'new tag');
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['mallSubId'] = this.mallSubId;
+    data['mallCategoryId'] = this.mallCategoryId;
+    data['mallSubName'] = this.mallSubName;
+    data['comments'] = this.comments;
+    return data;
+  }
+}
--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter01/service/service_method.dart';
-import '../model/category.dart';
+import '../model/category_model.dart';
+import "../element/r_log.dart";
 import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -11,10 +13,76 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
+    RLogger.instance.d('开始', tag: '_CategoryPageState');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("商品分类"),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            LeftCategoryNav(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//左侧大类导航
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+  @override
+  void initState() {
+    RLogger.instance.d('开始', tag: '_LeftCategoryNavState.initState');
     _getCategory();
+    super.initState();
+  }
+
+  List list = [];
+  @override
+  Widget build(BuildContext context) {
     return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            width: 1,
+            color: Colors.black12,
+          ),
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
       child: Container(
-        child: Text('分类'),
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color: Colors.black12),
+          ),
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(28),
+          ),
+        ),
       ),
     );
   }
@@ -23,9 +91,12 @@ class _CategoryPageState extends State<CategoryPage> {
     await request(
       url: 'getCategory',
     ).then((val) {
+      RLogger.instance.d('开始', tag: '_getCategory');
       var data = json.decode(val.toString());
-      print(data);
-      print(data['message']);
+
+      CategoryModel categoryList = CategoryModel.fromJson(data);
+      list = categoryList.data;
+      RLogger.instance.d('结束', tag: '_getCategory');
     });
   }
 }
